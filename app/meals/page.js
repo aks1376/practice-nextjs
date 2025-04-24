4193dcabe51fed1bd2c30842a1af02aa611596e7
Ali <alikarimi.s1376@gmail.com>
2025-04-24 12:43:48 +0330

    installed better-sqlite3 package added init sql db added meals items to meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,24 @@
-export default function MealsPage() {
+import Link from "next/link"
+import classes from "./page.module.css"
+import { getMeals } from "@/lib/meals"
+import { MealsGrid } from "@/components/meals/meals-grid";
+
+export default async function MealsPage() {
+
+  const meals = await getMeals()
+
   return <>
-    <h1>Meals page</h1>
+    <header className={classes.header}>
+      <h1>
+        Delicious meals created <span className={classes.highlight}>by you</span>
+      </h1>
+      <p>Choose your favorite recipe and cook by yourself. </p>
+      <p className={classes.cta}>
+        <Link href="/meals/share">Share your favorite recipe</Link>
+      </p>
+    </header>
+    <main className={classes.header}>
+      <MealsGrid meals={meals} />
+    </main>
   </>
 }
