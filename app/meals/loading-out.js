f19f4b46116cb0f3e88f9477ecd77c0934cb10f6
Ali <alikarimi.s1376@gmail.com>
2025-04-24 12:59:08 +0330

    added loading message while fetching meals

---
+++ app/meals/loading-out.js
@@ -0,0 +1,5 @@
+import classes from "./loading.module.css"
+
+export default function LoadingMealsPage() {
+  return <p className={classes.loading}>fetching meals...</p>
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,16 @@ import Link from "next/link"
 import classes from "./page.module.css"
 import { getMeals } from "@/lib/meals"
 import { MealsGrid } from "@/components/meals/meals-grid";
+import { Suspense } from "react";
+
+async function Meals() {
+  const meals = await getMeals()
+
+  return <MealsGrid meals={meals} />
+}
 
 export default async function MealsPage() {
 
-  const meals = await getMeals()
 
   return <>
     <header className={classes.header}>
@@ -18,7 +24,9 @@ export default async function MealsPage() {
       </p>
     </header>
     <main className={classes.header}>
-      <MealsGrid meals={meals} />
+      <Suspense fallback={<p className={classes.loading}>fetching meals...</p>}>
+        <Meals />
+      </Suspense>
     </main>
   </>
 }
