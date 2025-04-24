f5212395802e4bbbaac02a4342a6e7e2f42c4f7e
Ali <alikarimi.s1376@gmail.com>
2025-04-24 18:14:05 +0330

    added meal details page added not found meal data error

---
+++ app/meals/not-found.js
@@ -0,0 +1,9 @@
+
+export default function notFound() {
+  return <main className="not-found">
+    <h1>Meal not found</h1>
+    <p>
+      we could not find the requested page or meal.
+    </p>
+  </main>
+}
