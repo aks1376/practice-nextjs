a7c68d73a744e0dc823572abd4e8049b94aa4389
Ali <alikarimi.s1376@gmail.com>
2025-04-25 15:47:21 +0330

    added form input validation on server side

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,14 @@
+"use client"
+
 import { ImagePicker } from '@/components/meals/image-picker';
-import classes from './page.module.css';
-import { shareMeal } from '@/lib/action';
 import { MealsFormSubmit } from '@/components/meals/meals-form-submit';
+import { shareMeal } from '@/lib/action';
+import { useActionState } from 'react';
+import classes from './page.module.css';
 
 export default function ShareMealPage() {
+
+  const [state, formAction] = useActionState(shareMeal, { message: null })
 
   return (
     <>
@@ -14,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,11 +48,12 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your Image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
         </form>
       </main>
     </>
-  );
+  )
 }
